4c14e48d6ca34519d4c2ca94029f523f18e7b920
Aditya Kalambe <adityakalambe20@gmail.com>
2024-10-08 18:41:16 +0530

    Attribute Directives

---
+++ Learning Partner/06attribute_directives/src/app/atb-dir/atb-dir.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atb-dir',
+  standalone: true,
+  imports: [],
+  templateUrl: './atb-dir.component.html',
+  styleUrl: './atb-dir.component.css'
+})
+export class AtbDirComponent {
+
+}
